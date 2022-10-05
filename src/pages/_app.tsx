a6d6e3aade933358ba914a9d1f661474529e693b
Marcio Souza <marciosouzaunico@gmail.com>
2022-10-05 19:49:56 -0300

    up

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,23 @@
+import { AppProps } from "next/app"
+import { globalStyles } from "../styles/global"
 
+import logoImg from '../assets/Logo.svg'
+import Image from 'next/future/image'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import { Container, Header } from '../styles/pages/app'
+
+globalStyles();
+
+export default function App({ Component, pageProps }: AppProps) {
+
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg} alt="" />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  )
 }
 
-export default MyApp
+
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,27 @@
+import {styled} from'../styles'
 
+const Button = styled('button', {
+  backgroundColor: '$green300',
+  borderRadius: 4,
+  border: 0,
+  padding: '4px 8px',
+  cursor: 'pointer',
+
+  span: {
+    fontWeight: 'bold',
+  },
+
+  '&:hover': {
+    filter: 'brightness(0.5)'
+  },
+})
 
 export default function Home() {
   return (
-    <h1>hello world</h1>
+
+    <Button>
+      <span>teste</span>
+      Enviar
+    </Button>
   )
 }
